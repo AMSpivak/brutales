24bc1ca2e71587ad9fd38311572f5a4e5212618b
aьspivak <aspivak@mail.ru>
2022-08-16 20:14:14 +0300

    Shadow Scale fix

--- BruteForceEngine/main.cpp
+++ BruteForceEngine/main.cpp
@@ -197,10 +197,7 @@ void Update()
             atmosphere.m_SunShadow.y = shadow_tg_2;
             atmosphere.m_SunShadow.z = -cos(azimuth_rad);
             atmosphere.m_SunShadow.w = -sin(-azimuth_rad);
-            float z = abs(atmosphere.m_SunShadow.z);
-            float w = abs(atmosphere.m_SunShadow.w);
-            float sclr = z > w ? z : w;
-            atmosphere.m_SunShadowScaler = 1.0f / sclr;
+            atmosphere.m_SunShadowScaler = abs(atmosphere.m_SunShadow.z) + abs(atmosphere.m_SunShadow.w);
             chng = false;
         }
     }
